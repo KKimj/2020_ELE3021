c8bf20b12677c2f5f47b7065d0c48bf59056e359
Jiun Kim <ziun79@gmail.com>
2020-05-17 02:37:35 +0900

    modify proc.c

--- os_practice/xv6-public/proc.c
+++ os_practice/xv6-public/proc.c
@@ -477,7 +477,10 @@ scheduler(void)
   if(MLFQ_K == -1)
   panic("MLFQ_SCHED is -1 case");
 //struct mlfq _mlfq[MLFQ_K];
-
+struct proc *p;
+struct cpu *c = mycpu();
+c->proc = 0;
+  
 for(;;)
 {
   // 100 ticks 마다 초기화

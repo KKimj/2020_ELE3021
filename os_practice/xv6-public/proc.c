e4e340abb5de11d2615e1cdef73e8f0541d6b5dd
Jiun Kim <ziun79@gmail.com>
2020-05-17 00:56:09 +0900

    modify proc.c

--- os_practice/xv6-public/proc.c
+++ os_practice/xv6-public/proc.c
@@ -390,7 +390,64 @@ scheduler(void)
 #else
 
 #ifdef MULTILEVEL_SCHED
-  panic("MULTILEVEL_SCHED case");
+  //panic("MULTILEVEL_SCHED case");
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+  
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+    int n_pid_even = 0;
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+      if(p->pid % 2 == 1)
+        continue;
+      n_pid_even++;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    if(n_pid_even==0)
+    {
+      for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+      if(p->pid % 2 == 0)
+        continue;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    }
+    release(&ptable.lock);
+  }
+
 #endif
 
 #ifdef MLFQ_SCHED

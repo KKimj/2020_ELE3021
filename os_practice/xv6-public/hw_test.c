01d6daa52f708e50189ddfd0a72f8284b038d466
Jiun Kim <ziun79@gmail.com>
2020-05-16 23:18:11 +0900

    update hw_test.c

--- os_practice/xv6-public/hw_test.c
+++ os_practice/xv6-public/hw_test.c
@@ -8,6 +8,6 @@ int main(int argc, char *argv[])
     printf(1, "start\n");
     printf(1, "Call yield\n", yield());
     printf(1, "Call getlev return value : %d\n", getlev());
-    printf(1, "Call setpriority return value : \n", setpriority(1, 1));
+    printf(1, "Call setpriority return value : %d\n", setpriority(1, 1));
     exit();
 }

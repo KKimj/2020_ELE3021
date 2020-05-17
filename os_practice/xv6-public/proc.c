e9e6c85af7f84dbc0074a77452dd0bd38b464e96
Jiun Kim <ziun79@gmail.com>
2020-05-17 23:55:37 +0900

    modify proc.c

--- os_practice/xv6-public/proc.c
+++ os_practice/xv6-public/proc.c
@@ -576,8 +576,8 @@ scheduler(void)
         {
           if(__fq->level != cur_level-1)
             continue;
-          // if(__fq->ticks < 3)
-          //   _ch = 1;
+          if(__fq->ticks < 3)
+             _ch = 1;
         }
         if(_ch) 
         {

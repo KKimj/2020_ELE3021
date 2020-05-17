56c51cfc9debd50fdfd9e2305507e666aeadbc1e
Jiun Kim <ziun79@gmail.com>
2020-05-17 23:39:10 +0900

    modify proc.c

--- os_practice/xv6-public/proc.c
+++ os_practice/xv6-public/proc.c
@@ -574,10 +574,10 @@ scheduler(void)
         struct _mlfq * __fq;
         for(__fq = mlfq; __fq < & mlfq[NPROC]; __fq++)
         {
-          if(__fq->level != cur_level-1)
-            continue;
-          if(__fq->ticks < 3)
-            _ch = 1;
+          // if(__fq->level != cur_level-1)
+          //   continue;
+          // if(__fq->ticks < 3)
+          //   _ch = 1;
         }
         if(_ch) 
         {

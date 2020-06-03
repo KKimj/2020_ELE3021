5ab6d7617aeb81e68017fee79bd5dd96638a630d
KKimj <kkimj@hanyang.ac.kr>
2020-06-03 19:19:11 +0900

    pmanager.c

--- os_practice/xv6-public/pmanager.c
+++ os_practice/xv6-public/pmanager.c
@@ -2,15 +2,41 @@
 #include "stat.h"
 #include "user.h"
 
+
+// int getadmin(char *password);
+// int exec2(char *path, char **argv, int stacksize);
+// int setmemorylimit(int pid, int limit);
+// char * getshmem(int pid); 
+
+#define VERBOSE
+#define DEV
+
 int
 main(int argc, char *argv[])
 {
-    // char *buf = "Hello Pmanager.c";
-    // int ret_val;
-
+    #ifdef VERBOSE
     printf(1, "Pmanager Start!\n");
+    #endif
 
+    #ifdef DEV
+    char * passwd = "2018008159";
+    getadmin(passwd);
 
+    char * path = "./cat";
+    char * argv[] = {"Hello", "World!"};
+    int stacksize = 0;
+    exec2(path, argv, stacksize);
+
+    int pid = 0;
+    int limit = 0;
+    setmemorylimit(pid, limit);
+
+    getshmem(pid);
+    #endif
+
+    #ifdef VERBOSE
     printf(1, "Pmanager exit!\n");
+    #endif
     exit();
 }
+

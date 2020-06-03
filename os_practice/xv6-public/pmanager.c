e47438d308545435189221066b3cbe2811dfc937
KKimj <kkimj@hanyang.ac.kr>
2020-06-03 18:39:39 +0900

    1. User Program 추가

---
+++ os_practice/xv6-public/pmanager.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+    char *buf = "Hello Pmanager.c";
+    int ret_val;
+
+    printf(1, "Pmanager Start!\n");
+
+
+    printf(1, "Pmanager exit!\n");
+    exit();
+}

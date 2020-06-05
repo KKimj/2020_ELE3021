14bab1b8e8f68c34c20f03217b2e1cb5c51a4eba
KKimj <kkimj@hanyang.ac.kr>
2020-06-05 11:51:05 +0900

    My new test file myshmt.c

--- os_practice/xv6-public/sysproc.c
+++ os_practice/xv6-public/sysproc.c
@@ -188,7 +188,7 @@ sys_getshmem(void)
         return -1;
     char * res = getshmem(pid);
     #ifdef VERBOSE
-    cprintf("@sysproc.c -> getshmem return val : %p pid : %d sizeof(int) : %d sizeof(*char)\n", res, pid, sizeof(int), sizeof(char*));
+    cprintf("@sysproc.c -> getshmem return val : %p pid : %d sizeof(int) : %d sizeof(*char) %d\n", res, pid, sizeof(int), sizeof(char*));
     #endif
     return 1;
     return (int)res;

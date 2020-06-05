24d485401ab0916cc6876bc38215ae317afc4113
KKimj <kkimj@hanyang.ac.kr>
2020-06-05 21:48:04 +0900

    edit cast res

--- os_practice/xv6-public/myshmt.c
+++ os_practice/xv6-public/myshmt.c
@@ -2,11 +2,14 @@
 #include "stat.h"
 #include "user.h"
 
+#define N 4096
+
 int main(int argc, char *argv[])
 {
   int pid, parent;
   char *shmem;
   parent = getpid();
+  shmem = malloc(N);
 
   if ((pid = fork()) < 0)
   {
@@ -16,12 +19,13 @@ int main(int argc, char *argv[])
   if (pid == 0)
   {
     sleep(100);
-    shmem = getshmem(parent);
+    // shmem = getshmem(parent);
     printf(1, "A message from the parent  arddress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
   }
   else
   {
-    shmem = getshmem(parent);
+    // shmem = getshmem(parent);
+    
     // *shmem = 'H';
     printf(1, "parent adrress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
     wait();

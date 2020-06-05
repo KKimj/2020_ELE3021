aa5ee5413eab314a91bf0cf05b96ac67e487c88c
KKimj <kkimj@hanyang.ac.kr>
2020-06-06 01:47:46 +0900

    edit cast res

--- os_practice/xv6-public/p2_memory_test.c
+++ os_practice/xv6-public/p2_memory_test.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
   int total = 0;
   int pid = getpid();
   int i=0;
+  char * pre_ptr;
   printf(1, "Memory test: pid is %d\n", pid);
   for (;;)
   {
@@ -22,6 +23,10 @@ int main(int argc, char *argv[])
     total += N;
     printf(1, "Memory allocated: %d bytes\n", total);
     sleep(300);
+    if(pre_ptr)
+    printf(1, "test : last memory in : %c\n", *pre_ptr);
+
+    pre_ptr = ptr;
   }
   exit();
   return 0;

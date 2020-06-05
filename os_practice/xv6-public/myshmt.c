7c0d73b6de02c989a82bb2f27272d09994875123
KKimj <kkimj@hanyang.ac.kr>
2020-06-05 11:36:49 +0900

    My new test file myshmt.c

---
+++ os_practice/xv6-public/myshmt.c
@@ -0,0 +1,34 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+  int pid, parent;
+  char *shmem;
+  parent = getpid();
+
+  if ((pid = fork()) < 0)
+  {
+    printf(1, "fork failed!\n");
+    exit();
+  }
+  if (pid == 0)
+  {
+    sleep(100);
+    shmem = getshmem(parent);
+    printf(1, "A message from the parent process: %c\n", *shmem);
+  }
+  else
+  {
+    shmem = getshmem(parent);
+    *shmem = 'H';
+
+    wait();
+    
+  }
+
+  exit();
+  return 0;
+}
+

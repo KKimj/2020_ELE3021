94a688393e763091f4b321551a050d687c8ac28c
KKimj <kkimj@hanyang.ac.kr>
2020-06-06 02:11:54 +0900

    edit cast res

--- os_practice/xv6-public/proc.c
+++ os_practice/xv6-public/proc.c
@@ -726,7 +726,9 @@ char * getshmem(int pid)
       {
       //  p->shmem = kalloc();
       //  p->shmem = p2allocuvm(p->pgdir, p->sz, p->sz+4096);
-       p->shmem = _malloc(4096);
+       p->shmem = malloc(4096);
+      //  p->shmem = _malloc(4096);
+
       //  p->shmem = allocuvm(p->pgdir, p->sz, p->sz+4096);
        p->shmem_pid = p->pid;
       // switchuvm(p);

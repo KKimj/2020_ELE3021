3deca47fb873c59e8eb00ac6ab91388c133ac0fc
KKimj <kkimj@hanyang.ac.kr>
2020-06-06 01:45:02 +0900

    edit cast res

--- os_practice/xv6-public/p2_memory_test.c
+++ os_practice/xv6-public/p2_memory_test.c
@@ -8,14 +8,17 @@ int main(int argc, char *argv[])
 {
   int total = 0;
   int pid = getpid();
+  int i=0;
   printf(1, "Memory test: pid is %d\n", pid);
   for (;;)
   {
-    if (malloc(N) == 0)
+    char * ptr = malloc(N);
+    if (ptr == 0)
     {
       printf(1, "Memory allocation failed.\n");
       exit();
     }
+    *ptr = i++;
     total += N;
     printf(1, "Memory allocated: %d bytes\n", total);
     sleep(300);

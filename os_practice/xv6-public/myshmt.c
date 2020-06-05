4233f730f18a9bb0d0b34f518e12d58a3b86a9bc
KKimj <kkimj@hanyang.ac.kr>
2020-06-05 11:53:09 +0900

    My new test file myshmt.c

--- os_practice/xv6-public/myshmt.c
+++ os_practice/xv6-public/myshmt.c
@@ -17,13 +17,13 @@ int main(int argc, char *argv[])
   {
     sleep(100);
     shmem = getshmem(parent);
-    printf(1, "A message from the parent  arddress : %p char : %c\n", shmem, *shmem);
+    printf(1, "A message from the parent  arddress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
   }
   else
   {
     shmem = getshmem(parent);
     // *shmem = 'H';
-    printf(1, "parent adrress : %d char : %c\n", shmem, *shmem);
+    printf(1, "parent adrress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
     wait();
     
   }

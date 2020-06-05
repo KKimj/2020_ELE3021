8f9c00dcfff4d027cb747b8ac7de489505b53d3d
KKimj <kkimj@hanyang.ac.kr>
2020-06-05 21:51:15 +0900

    edit cast res

--- os_practice/xv6-public/myshmt.c
+++ os_practice/xv6-public/myshmt.c
@@ -22,14 +22,14 @@ int main(int argc, char *argv[])
   {
     sleep(100);
     // shmem = getshmem(parent);
-    printf(1, "A message from the parent  arddress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
+    printf(1, "@Child -> Memory arddress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
   }
   else
   {
     // shmem = getshmem(parent);
     
     *shmem = 'H';
-    printf(1, "parent adrress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
+    printf(1, "@Parent -> Memory adrress : %p address cast to int %d char : %c\n", shmem, shmem, *shmem);
     wait();
     
   }
